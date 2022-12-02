a3722878d4aa248a7fe785d28535921f6be0a7d0
Will Stenzel <wstenzel@stu.naperville203.org>
2022-12-02 07:37:26 -0600

    ?

--- Advent22/Advent22/Day1.java
+++ Advent22/Advent22/Day1.java
@@ -1,1 +1,44 @@
-import java.util.scanner;
+import java.util.Scanner;
+import java.io.*;
+public class Day1
+{
+    public static void main(String[] args) throws IOException
+    {
+        Scanner file = new Scanner(new File("calories.txt"));
+        int check1;
+        String check2;
+        int check3 = 0;
+        boolean check = false;
+        int sum=0;
+        int top=0;
+        
+        while(file.hasNext())
+        {
+            check1 = file.nextInt();
+            check2 = file.nextLine();
+            if(check2.equals(""))
+            {
+                check = false;
+            }
+            else
+                check3 = Integer.parseInt(check2);
+            if(check1==check3)
+            {
+                check = true;
+            }
+            if(check)
+            {
+                sum = sum + check1;
+            }
+            else
+            {
+                sum = 0;
+            }
+            if (sum>top)
+            {
+                top = sum;
+            }
+        }
+        System.out.print(top);
+    }
+}
