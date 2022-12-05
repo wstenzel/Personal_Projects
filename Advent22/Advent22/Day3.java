fcfd7e33368fdc92db9679ef419e7c8e7f8568cb
Will Stenzel <wstenzel@stu.naperville203.org>
2022-12-05 15:08:05 -0600

    babfdabgfds

---
+++ Advent22/Advent22/Day3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Day3
+{
+    public static void main(String[] args) throws IOException
+    {
+        Scanner file = new Scanner(new File("Day3.txt"));
+        String item;
+        String l1,l2;
+        while(file.hasNext())
+        {
+            item = file.nextLine();
+            int len = item.length()-1;
+            
+        }
+        
+    }
+}
