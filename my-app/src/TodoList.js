c936e0608099d73099d171d58ab3ce4102f0d08b
Will Stenzel <wstenzel@stu.naperville203.org>
2023-11-30 22:42:53 -0600

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ my-app/src/TodoList.js
@@ -0,0 +1,21 @@
+const TodoList = ({list, remove}) => {
+    return (
+      <>
+        {list?.length > 0 ? (
+          <ul className = "todo-list">
+            {list.map((entry, index) => (
+              <div className="todo">
+                <li key={index}>{entry}</li>
+                <button className = "delete-button" onClick={() => {remove(entry);}}>X</button>
+              </div>
+            ))}
+          </ul>
+        ) : (
+          <div className = "empty">
+            <p>No Tasks Found</p>
+          </div>
+        )}
+      </>
+    );
+  };
+export default TodoList;
